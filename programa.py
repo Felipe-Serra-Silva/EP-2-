92ebda1f491c7f2fd11b760a5c1c263a9c740816
Felipe Serra <felipeserrasilva.com@gmail.com>
2024-10-25 16:35:03 -0300

    Programa e funções finais

--- programa.py
+++ programa.py
@@ -82,16 +82,8 @@ frota_oponente = {
 tabuleiro_oponente = posiciona_frota(frota_oponente)
 tabuleiro_jogador = posiciona_frota(dicionario_frota)
 
-def monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente):
-        texto = ''
-        texto += '   0  1  2  3  4  5  6  7  8  9         0  1  2  3  4  5  6  7  8  9\n'
-        texto += '_______________________________      _______________________________\n'
-
-        for linha in range(len(tabuleiro_jogador)):
-            jogador_info = '  '.join([str(item) for item in tabuleiro_jogador[linha]])
-            oponente_info = '  '.join([info if str(info) in 'X-' else '0' for info in tabuleiro_oponente[linha]])
-            texto += f'{linha}| {jogador_info}|     {linha}| {oponente_info}|\n'
-        return texto
+# a função monta_tabuleiros foi adicionada no arquivo: funcoes.py
+# Printa o tabuleiro atualizado:
 print(monta_tabuleiros(tabuleiro_jogador,tabuleiro_oponente))
 
 jogando = True
